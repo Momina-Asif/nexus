2b5fee9953ac6b5aa7d9fff110af5211a51e33fc
Momina Asif <112888467+Momina-Asif@users.noreply.github.com>
2024-10-12 20:40:20 +0500

    Make module of Authorization

--- nexus_backend/nexus/urls.py
+++ nexus_backend/nexus/urls.py
@@ -1,19 +1,8 @@
-# from django.urls import path
-# from .api import api  # Import the NinjaAPI instance
-# from .views import signup, login,  protected_view
-
-# urlpatterns = [
-#     path("api/", api.urls), 
-#     path('api/signup/', signup, name='signup'),
-#     path('api/login/', login, name='login'),
-#     path('api/token/', api.urls),  # Add the token routes
-# ]
-
 # urls.py
 
 from django.urls import path
-from .api import api  # Import the NinjaAPI instance
+from .auth import auth_router as authapis
 
 urlpatterns = [
-    path("api/", api.urls),  # This will include all routes defined in api.py
+    path("auth/", authapis.urls),
 ]
--- nexus_backend/nexus_backend/settings.py
+++ nexus_backend/nexus_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'ninja',
     'rest_framework_simplejwt',
+    'ninja_jwt',
     
 ]
 
@@ -150,3 +151,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True, 
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+ALLOWED_HOSTS = [
+    "localhost", "*"
+]
